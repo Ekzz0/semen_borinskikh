447d607ad71f417b3463e47173732e4831886d7c
Ekzz0 <92272824+Ekzz0@users.noreply.github.com>
2021-12-06 00:36:19 +0300

    Update hw1.cpp

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -1,113 +1,93 @@
 #include <iostream>
+#include <string>
 #include <fstream>
-#include <iomanip>
-#include <valarray>
+#include <cmath>
 
 using namespace std;
-string * StringToMass(string base_str, char delim, int size){
 
-    size_t pos = 0;
-    size_t base_str_size = base_str.size();
-    //size_t delim_size = delim.size();
-    size_t delim_size=1;
-    string temp;
-    int i=0;
-    string *mass_str = new string[size];
-    while (pos < base_str_size) {
-        temp = temp.assign(base_str, pos, base_str.find(delim, pos) - pos);
-        if (temp.size() > 0)  // проверка на пустую строку при необходимости
-        {
-           // cout << temp << endl;
-            mass_str[i] = temp;
-            i++;
-            if(i==size){
-                break;
-            }
-            //cout << i << endl;
-        }
-        pos += temp.size() + delim_size;
 
-    }
-
-    return mass_str;
+float get_x(ifstream &file_in) // функция получения координаты x
+{
+    string x_cord;
+    float x;
+    file_in >> x_cord;
+    x = stof(x_cord);
+    return x;
 }
-std::tuple<int, int> step(int dividend, int divisor) {
-    return  std::make_tuple(dividend / divisor, dividend % divisor);
+
+float get_y(ifstream &file_in)  // функция получения координаты y
+{
+    string y_cord;
+    float y;
+    file_in >> ws >> y_cord;
+    y = stof(y_cord);
+    return y;
 }
+
 int main() {
-    std::string line;
-    double xn,yn,fx_r,fy_r,fx_l,fy_l;
-    double finde_value=1;
-    double max_d_r=0;
-    double max_d_l=0;
-    bool isFirstLine= true;
-    bool isTwiceLine_r= true,isTwiceLine_l= true;
-    std::ifstream in("in.txt"); // окрываем файл для чтения
-    if (in.is_open())
-    {
-        double x=0; double y=0;
-        in >> x >> y;
-        xn=(-1)*x;
-        yn=(-1)*y;
-        //std::<<  <<::endl;
-        while (in >> x >> y)
-        {
-                //растояние
-                double dist;
-                if(xn !=0 && yn !=0){
-                    dist=(abs(-1*(x)/xn+y/yn)/sqrt((1/xn)*(1/xn)+(1/yn)*(1/yn)));
-					dist = std::round(dist * 10000000000.0) / 10000000000.0;
-                   // dist=(abs((yn-0)*x-(xn-0)*y+xn*0-yn*0))/(sqrt(pow(xn-0,2)+pow(yn-0,2))); //вектор как прямая заданная 2мя точками
-                }
-                else{
-                    (xn ==0)? dist =x:dist= y;
-                    /*if(xn ==0){
-                        dist= x;
-                        //dist=sqrt(pow(x-xn,2)+pow(y-yn,2)); //как растояние между 2мя точками
-                    }
-                    else{
-                        dist=y;
-                    }*/
-                }
-                //<0 - справа:
-                if(((xn-0)*(y-0)-(yn-0)*(x-0))>=0){
-                    if(dist>=max_d_r){
-                        max_d_r=dist;
-                        fx_r=x;
-                        fy_r=y;
-                    }
-                    /*if(isTwiceLine_r){
-                        isTwiceLine_r= false;
-                        max_d_r=dist;
-                        fx_r=x;
-                        fy_r=y;
-                    }
-                    else{
-                    }*/
-                }//>0 - слева
-                else{
-                    if(dist>=max_d_l){
-                        max_d_l=dist;
-                        fx_l=x;
-                        fy_l=y;
-                    }
+    double x_max_left = 0;
+    double y_max_left = 0;
+    double x_max_right = 0;
+    double y_max_right = 0;
+    double max_r = 0; // Для правого
+    double max_l = 0; // Для левого
+    double Dist = 0 ;
 
-                    /*if(isTwiceLine_l){
-                        isTwiceLine_l= false;
-                        max_d_l=dist;
-                        fx_l=x;
-                        fy_l=y;
-                    }
-                    else{
-                    }*/
-                }
+    ifstream file_in("in.txt");
 
-            //delete[] values;
-        }
+    if (!file_in.is_open()) {
+        //cout << "Error " << endl;
     }
-    in.close();     // закрываем файл
-    std::cout << "Leftmost: " << fx_l << " " << fy_l << "\n";
-    std::cout << "Rightmost: " << fx_r << " " << fy_r << "\n";
+    else {
+        //cout << "Successful " << endl;
+        int x = 0;
+        int y = 0;
 
+        int xn = get_x(file_in);
+        int yn =  get_y(file_in);
+
+        while (!file_in.eof()) {
+            if(xn !=0 && yn !=0){
+                Dist = abs(xn * y - yn * x) / sqrt(xn ^ 2 + yn ^ 2);
+            }
+
+            x = get_x(file_in);
+            y = get_y(file_in);
+            // Проверка на максимальный элемент + вычисление длины от вектора до точки
+
+
+            if (xn * y - yn *x  > 0.0 ) {        // слева
+
+                if (Dist > max_l) {
+                    max_l = Dist;
+                    x_max_left = x; // x_max_right
+                    y_max_left = y; // y_max_right
+                }
+            }
+            else if (xn * y - yn * x == 0.0) {
+
+                if (Dist > max_r) {
+                    max_r = Dist;
+                    x_max_right = x; // x_max_left
+                    y_max_right = y; // y_max_left
+                }
+            }
+            else {          // справа
+                Dist  = abs(xn * y - yn * x) / sqrt(xn ^ 2 + yn ^ 2);
+                if (Dist > max_r) {
+                    max_r = Dist;
+                    x_max_right = x; // x_max_left
+                    y_max_right = y; // y_max_left
+                }
+            }
+        }
+
+        }
+    file_in.close();
+    cout << "Leftmost: " << x_max_left << ' ' << y_max_left << endl;
+    cout << "Rightmost: " <<  x_max_right << ' ' << y_max_right << endl;
     return 0;
-}
+    }
+
+
+
